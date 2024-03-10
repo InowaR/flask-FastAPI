026333c6e1bb147fc0609fea207fd84b0cc63984
georgiy <georgii_sidenko@mail.ru>
2024-03-10 07:22:47 +0300

    task8

--- lesson1/homework1/task8.py
+++ lesson1/homework1/task8.py
@@ -1,11 +1,28 @@
-from flask import Flask
+# Создать базовый шаблон для всего сайта, содержащий
+# общие элементы дизайна (шапка, меню, подвал), и
+# дочерние шаблоны для каждой отдельной страницы.
+# Например, создать страницу "О нас" и "Контакты",
+# используя базовый шаблон.
+
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return 'hello'
+    return render_template('base.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 
 if __name__ == '__main__':
