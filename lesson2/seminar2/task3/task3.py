87b0bf4c4a31a8eb6d7b25942e40538376c9c94f
georgiy <georgii_sidenko@mail.ru>
2024-03-11 07:19:25 +0300

    fixed login and password

--- lesson2/seminar2/task3/task3.py
+++ lesson2/seminar2/task3/task3.py
@@ -3,26 +3,30 @@
 # и переход на страницу приветствия пользователя или страницу с ошибкой.
 
 
-from flask import Flask, render_template, request, redirect, url_for, flash
+import secrets
+from flask import Flask, render_template, request, redirect, url_for, flash, session
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = secrets.token_hex()
 
 
 @app.route('/')
 def login_password():
+    _login = session.get('_login')
+    print(_login)
     return render_template('login_password.html')
 
 
 @app.route('/profile', methods=['GET', 'POST'])
 def profile():
-    if request.method == 'POST':
-        _login = request.form['login']
-        _password = request.form['password']
-        if _login == 'admin' and _password == '12345':
-            return render_template('profile.html', _login=_login)
-        else:
-            flash('Неверный логин или пароль', 'error')
-            return redirect(url_for('login_password'))
+    _login = request.form['login']
+    _password = request.form['password']
+    if _login == 'admin' and _password == '12345':
+        flash('Вы успешно зарегистрировались!', category='success')
+        return render_template('profile.html', _login=_login)
+    else:
+        flash('Неверный логин или пароль', category='error')
+        return redirect(url_for('login_password'))
 
 
 if __name__ == '__main__':
