a660fc2392300b312dbfa2a989d1fa1524c9cacc
georgiy <georgii_sidenko@mail.ru>
2024-03-13 20:00:47 +0300

    2 homework task 5

--- lesson2/homework2/task5/task5.py
+++ lesson2/homework2/task5/task5.py
@@ -4,3 +4,38 @@
 # При нажатии на кнопку будет произведено вычисление
 # результата выбранной операции и переход на страницу с
 # результатом.
+
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/result', methods=['POST'])
+def result():
+    num1 = float(request.form['num1'])
+    num2 = float(request.form['num2'])
+    operation = request.form['operation']
+    res = 0
+    if operation == '+':
+        res = num1 + num2
+    elif operation == '-':
+        res = num1 - num2
+    elif operation == '*':
+        res = num1 * num2
+    elif operation == '/':
+        try:
+            res = num1 / num2
+        except ZeroDivisionError:
+            res = 0
+
+    return render_template('result.html', operation=operation, result=res)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
