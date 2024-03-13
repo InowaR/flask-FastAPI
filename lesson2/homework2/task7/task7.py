2b0e851afc5b8dc2b29e64057d415491c256734e
georgiy <georgii_sidenko@mail.ru>
2024-03-13 20:29:09 +0300

    2 homework task 7

--- lesson2/homework2/task7/task7.py
+++ lesson2/homework2/task7/task7.py
@@ -3,3 +3,23 @@
 # При нажатии на кнопку будет произведено
 # перенаправление на страницу с результатом, где будет
 # выведено введенное число и его квадрат.
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/result', methods=['POST'])
+def result():
+    number = int(request.form['number'])
+    square = number ** 2
+    return render_template('result.html', number=number, square=square)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
