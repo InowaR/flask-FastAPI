f39b1204572ac2a58b662444f3f4478b55dde44c
georgiy <georgii_sidenko@mail.ru>
2024-03-13 20:06:31 +0300

    2 homework task 6

--- lesson2/homework2/task6/task6.py
+++ lesson2/homework2/task6/task6.py
@@ -3,3 +3,28 @@
 # При нажатии на кнопку будет произведена проверка
 # возраста и переход на страницу с результатом или на
 # страницу с ошибкой в случае некорректного возраста.
+
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/result', methods=['POST'])
+def result():
+    name = request.form['name']
+    age = int(request.form['age'])
+
+    if age < 18:
+        return render_template('error.html', name=name)
+    else:
+        return render_template('success.html', name=name)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
