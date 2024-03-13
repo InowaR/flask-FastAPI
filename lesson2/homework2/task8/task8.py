e0386fa8b8bd6a97b519487b3e5360fae013d073
georgiy <georgii_sidenko@mail.ru>
2024-03-13 20:40:31 +0300

    2 homework task 8

--- lesson2/homework2/task8/task8.py
+++ lesson2/homework2/task8/task8.py
@@ -3,3 +3,25 @@
 # При нажатии на кнопку будет произведено
 # перенаправление на страницу с flash сообщением, где будет
 # выведено "Привет, {имя}!".
+
+import secrets
+from flask import Flask, render_template, request, redirect, flash
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = secrets.token_hex()
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/result', methods=['POST'])
+def result():
+    name = request.form['name']
+    flash(f"Привет, {name}!")
+    return redirect('/')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
