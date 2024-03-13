565195ddc9fa3922b94e2ca0cc8b7fd46e1853a1
georgiy <georgii_sidenko@mail.ru>
2024-03-13 19:49:03 +0300

    2 homework task 4

--- lesson2/homework2/task4/task4.py
+++ lesson2/homework2/task4/task4.py
@@ -1,23 +1,25 @@
+# Создать страницу, на которой будет форма для ввода текста и
+# кнопка "Отправить"
+# При нажатии кнопки будет произведен подсчет количества слов
+# в тексте и переход на страницу с результатом.
 
-from flask import Flask, render_template, request, redirect, url_for
+
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def login_password():
-    return render_template('login_password.html')
+def index():
+    return render_template('index.html')
 
 
-@app.route('/profile', methods=['POST'])
-def profile():
-    _login = request.form['login']
-    _password = request.form['password']
-    if _login == 'admin' and _password == '12345':
-        return render_template('profile.html', _login=_login, _password=_password)
-    else:
-        return redirect(url_for('login_password'))
+@app.route('/result', methods=['POST'])
+def result():
+    text = request.form['text']
+    word_count = len(text.split())
+    return render_template('result.html', word_count=word_count)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
